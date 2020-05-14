65dff0ea2178e888dc875fb69dcb4062291ba48f
bulletmys <bulletmys@yandex.ru>
2020-05-14 22:38:22 +0300

    [ADD] base url

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -68,27 +68,27 @@ func StartNew() {
 
 	service := delivery.ServiceHandler{ServiceRepo: repository.NewDBServiceRepository(conn)}
 
-	m.HandleFunc("/user/{nickname}/create", user.Create).Methods("POST")
-	m.HandleFunc("/user/{nickname}/profile", user.Find).Methods("GET")
-	m.HandleFunc("/user/{nickname}/profile", user.Update).Methods("POST")
+	m.HandleFunc("/api/user/{nickname}/create", user.Create).Methods("POST")
+	m.HandleFunc("/api/user/{nickname}/profile", user.Find).Methods("GET")
+	m.HandleFunc("/api/user/{nickname}/profile", user.Update).Methods("POST")
 
-	m.HandleFunc("/forum/create", forum.Create).Methods("POST")
-	m.HandleFunc("/forum/{slug}/details", forum.Find).Methods("GET")
-	m.HandleFunc("/forum/{slug}/create", thread.Create).Methods("POST")
-	m.HandleFunc("/forum/{slug}/threads", thread.GetThreadsByForum).Methods("GET")
-	m.HandleFunc("/forum/{slug}/users", forum.GetForumUsers).Methods("GET")
+	m.HandleFunc("/api/forum/create", forum.Create).Methods("POST")
+	m.HandleFunc("/api/forum/{slug}/details", forum.Find).Methods("GET")
+	m.HandleFunc("/api/forum/{slug}/create", thread.Create).Methods("POST")
+	m.HandleFunc("/api/forum/{slug}/threads", thread.GetThreadsByForum).Methods("GET")
+	m.HandleFunc("/api/forum/{slug}/users", forum.GetForumUsers).Methods("GET")
 
-	m.HandleFunc("/thread/{slug_or_id}/create", post.Create).Methods("POST")
-	m.HandleFunc("/thread/{slug_or_id}/vote", thread.Vote).Methods("POST")
-	m.HandleFunc("/thread/{slug_or_id}/details", thread.Get).Methods("GET")
-	m.HandleFunc("/thread/{slug_or_id}/details", thread.Update).Methods("POST")
-	m.HandleFunc("/thread/{slug_or_id}/posts", post.Find).Methods("GET")
+	m.HandleFunc("/api/thread/{slug_or_id}/create", post.Create).Methods("POST")
+	m.HandleFunc("/api/thread/{slug_or_id}/vote", thread.Vote).Methods("POST")
+	m.HandleFunc("/api/thread/{slug_or_id}/details", thread.Get).Methods("GET")
+	m.HandleFunc("/api/thread/{slug_or_id}/details", thread.Update).Methods("POST")
+	m.HandleFunc("/api/thread/{slug_or_id}/posts", post.Find).Methods("GET")
 
-	m.HandleFunc("/post/{id}/details", post.GetDetails).Methods("GET")
-	m.HandleFunc("/post/{id}/details", post.Update).Methods("POST")
+	m.HandleFunc("/api/post/{id}/details", post.GetDetails).Methods("GET")
+	m.HandleFunc("/api/post/{id}/details", post.Update).Methods("POST")
 
-	m.HandleFunc("/service/status", service.Status).Methods("GET")
-	m.HandleFunc("/service/clear", service.Clear).Methods("POST")
+	m.HandleFunc("/api/service/status", service.Status).Methods("GET")
+	m.HandleFunc("/api/service/clear", service.Clear).Methods("POST")
 
 	fmt.Println("starting server at :5000")
 

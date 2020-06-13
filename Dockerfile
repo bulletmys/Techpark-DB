f243cab5d0f104adc1702a02715232a03bc385f3
bulletmys <bulletmys@yandex.ru>
2020-06-14 00:48:51 +0300

    [UPD] some optimization 5

--- Dockerfile
+++ Dockerfile
@@ -37,10 +37,18 @@ RUN /etc/init.d/postgresql start &&\
 RUN echo "local all all md5" > /etc/postgresql/$PGVER/main/pg_hba.conf && \
     echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/$PGVER/main/pg_hba.conf
 
+#RUN echo "listen_addresses='*'" >> /etc/postgresql/$PGVER/main/postgresql.conf
+#
+#RUN echo "synchronous_commit='off'" >> /etc/postgresql/$PGVER/main/postgresql.conf
+#RUN echo "fsync = 'off'" >> /etc/postgresql/$PGVER/main/postgresql.conf
 RUN echo "listen_addresses='*'" >> /etc/postgresql/$PGVER/main/postgresql.conf
-
 RUN echo "synchronous_commit='off'" >> /etc/postgresql/$PGVER/main/postgresql.conf
 RUN echo "fsync = 'off'" >> /etc/postgresql/$PGVER/main/postgresql.conf
+RUN echo "work_mem = 16MB" >> /etc/postgresql/$PGVER/main/postgresql.conf
+RUN echo "effective_cache_size = 1024MB" >> /etc/postgresql/$PGVER/main/postgresql.conf
+RUN echo "shared_buffers = 256MB" >> /etc/postgresql/$PGVER/main/postgresql.conf
+RUN echo "max_wal_size = 1GB" >> /etc/postgresql/$PGVER/main/postgresql.conf
+RUN echo "logging_collector = 'off'" >> /etc/postgresql/$PGVER/main/postgresql.conf
 
 EXPOSE 5432
 VOLUME  ["/etc/postgresql", "/var/log/postgresql", "/var/lib/postgresql"]
--- sql/init.sql
+++ sql/init.sql
@@ -204,36 +204,36 @@ CREATE INDEX IF NOT EXISTS index_posts_thread_path ON posts (thread, path);
 -- WHERE
 --   idx_scan = 0
 --   AND indisunique is false;
-
-ALTER SYSTEM SET
-    max_connections = '200';
-ALTER SYSTEM SET
-    shared_buffers = '256MB';
-ALTER SYSTEM SET
-    effective_cache_size = '768MB';
-ALTER SYSTEM SET
-    maintenance_work_mem = '64MB';
-ALTER SYSTEM SET
-    checkpoint_completion_target = '0.7';
-ALTER SYSTEM SET
-    wal_buffers = '7864kB';
-ALTER SYSTEM SET
-    default_statistics_target = '100';
-ALTER SYSTEM SET
-    random_page_cost = '1.1';
-ALTER SYSTEM SET
-    effective_io_concurrency = '200';
-ALTER SYSTEM SET
-    work_mem = '1310kB';
-ALTER SYSTEM SET
-    min_wal_size = '1GB';
-ALTER SYSTEM SET
-    max_wal_size = '4GB';
-ALTER SYSTEM SET
-    max_worker_processes = '2';
-ALTER SYSTEM SET
-    max_parallel_workers_per_gather = '1';
-ALTER SYSTEM SET
-    max_parallel_workers = '2';
-ALTER SYSTEM SET
-    max_parallel_maintenance_workers = '1';
+--
+-- ALTER SYSTEM SET
+--     max_connections = '200';
+-- ALTER SYSTEM SET
+--     shared_buffers = '256MB';
+-- ALTER SYSTEM SET
+--     effective_cache_size = '1024MB';
+-- ALTER SYSTEM SET
+--     maintenance_work_mem = '64MB';
+-- ALTER SYSTEM SET
+--     checkpoint_completion_target = '0.7';
+-- ALTER SYSTEM SET
+--     wal_buffers = '7864kB';
+-- ALTER SYSTEM SET
+--     default_statistics_target = '100';
+-- ALTER SYSTEM SET
+--     random_page_cost = '1.1';
+-- ALTER SYSTEM SET
+--     effective_io_concurrency = '200';
+-- ALTER SYSTEM SET
+--     work_mem = '1310kB';
+-- ALTER SYSTEM SET
+--     min_wal_size = '1GB';
+-- ALTER SYSTEM SET
+--     max_wal_size = '4GB';
+-- ALTER SYSTEM SET
+--     max_worker_processes = '2';
+-- ALTER SYSTEM SET
+--     max_parallel_workers_per_gather = '1';
+-- ALTER SYSTEM SET
+--     max_parallel_workers = '2';
+-- ALTER SYSTEM SET
+--     max_parallel_maintenance_workers = '1';
